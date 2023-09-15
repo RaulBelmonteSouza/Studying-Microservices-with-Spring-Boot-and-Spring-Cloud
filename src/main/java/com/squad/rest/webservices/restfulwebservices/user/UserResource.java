8742005484e124fa213d63b1232a6809a81afa11
RaulBelmonteSouza <raulbelmontedesouza@gmail.com>
2023-09-15 19:45:19 -0300

    Handling user not found exception

--- src/main/java/com/squad/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/squad/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,12 +3,7 @@ package com.squad.rest.webservices.restfulwebservices.user;
 import java.net.URI;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
@@ -28,7 +23,9 @@ public class UserResource {
 
   @GetMapping("/{id}")
   public User retrieveUser(@PathVariable Integer id) {
-    return service.findOne(id);
+    User user = service.findOne(id);
+    if(user == null) throw new UserNotFoundException("ID: " + id);
+    return user;
   }
 
   @PostMapping
