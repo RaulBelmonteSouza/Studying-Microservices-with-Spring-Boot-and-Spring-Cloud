c3820712acd8e6bab6786a7beb1f00f59f1d2f40
RaulBelmonteSouza <raulbelmontedesouza@gmail.com>
2024-01-08 16:31:50 -0300

    Adding and configuring JPA with Hibernate to the currency exchange service

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into currency_exchange(currency_from, currency_to, conversion_multiple, environment)
+values('USD', 'BRL', '65', '');
+
+insert into currency_exchange(currency_from, currency_to, conversion_multiple, environment)
+values('USD', 'EUR', '5', '');
+
+insert into currency_exchange(currency_from, currency_to, conversion_multiple, environment)
+values('USD', 'AUD', '25', '');
